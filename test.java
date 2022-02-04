f9149eb963448d48585c1b42d21c377f705cfb0a
CMasterM <97553498+CMasterM@users.noreply.github.com>
2022-02-04 12:45:48 -0800

    libs and code for test

--- test.java
+++ test.java
@@ -1,1 +1,6 @@
-
+import java.math.*;
+public class test{
+    public static double pythag(double a, double b){
+        return Math.sqrt(Math.pow(a, 2)+ Math.pow(b, 2));
+    }
+}
---
+++ testTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+import org.junit.Assert.*;
+
+public class testTester {
+    @Test
+    public void testPythag(){
+        assertEquals(6, test.pythag(3, 4));
+    }
+}
